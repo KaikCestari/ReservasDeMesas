c63b0d35a6e47479d385ca4a16dbacab541d8109
Kaik Cestari <kaikcestaridev@gmail.com>
2025-08-18 17:16:20 -0300

    feat: adicionei o spring security e confgurei para liberar acesso para o h2

--- src/main/java/com/devkaik7/ReservasMesas/Config/SecurityConfig.java
+++ src/main/java/com/devkaik7/ReservasMesas/Config/SecurityConfig.java
@@ -1,4 +1,30 @@
 package com.devkaik7.ReservasMesas.Config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
 public class SecurityConfig {
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/h2-console/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .csrf(csrf -> csrf.disable()) // necessário para H2
+                .headers(headers -> headers.frameOptions(frame -> frame.disable())) // H2 precisa
+                .formLogin(form -> form.loginPage("/login").permitAll()); // nova forma de habilitar login
+
+        return http.build();
+    }
+
+
 }
