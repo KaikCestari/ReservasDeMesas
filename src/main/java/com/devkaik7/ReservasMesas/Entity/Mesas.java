e8f24e7995e55b0e683944deec7e150b0817a61f
Kaik Cestari <kaikcestaridev@gmail.com>
2025-08-19 11:21:47 -0300

    feat: adc entity mesas

---
+++ src/main/java/com/devkaik7/ReservasMesas/Entity/Mesas.java
@@ -0,0 +1,46 @@
+package com.devkaik7.ReservasMesas.Entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_mesas")
+public class Mesas {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long numero;
+    private Long capacidade;
+
+    public Mesas() {
+    }
+
+    public Mesas(Long id, Long numero, Long capacidade) {
+        this.id = id;
+        this.numero = numero;
+        this.capacidade = capacidade;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getNumero() {
+        return numero;
+    }
+
+    public void setNumero(Long numero) {
+        this.numero = numero;
+    }
+
+    public Long getCapacidade() {
+        return capacidade;
+    }
+
+    public void setCapacidade(Long capacidade) {
+        this.capacidade = capacidade;
+    }
+}
