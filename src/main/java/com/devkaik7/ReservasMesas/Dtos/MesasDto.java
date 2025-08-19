1b39a8f75648ea0e51dddd2f1618b5cc40f49787
Kaik Cestari <kaikcestaridev@gmail.com>
2025-08-19 11:38:10 -0300

    feat: MesaDto add

---
+++ src/main/java/com/devkaik7/ReservasMesas/Dtos/MesasDto.java
@@ -0,0 +1,30 @@
+package com.devkaik7.ReservasMesas.Dtos;
+
+public class MesasDto {
+    private Long numero;
+    private Long capacidade;
+
+    public MesasDto() {
+    }
+
+    public MesasDto(Long numero, Long capacidade) {
+        this.numero = numero;
+        this.capacidade = capacidade;
+    }
+
+    public Long getNumero() {
+        return numero;
+    }
+
+    public void setNumero(Long numero) {
+        this.numero = numero;
+    }
+
+    public Long getCapacidade() {
+        return capacidade;
+    }
+
+    public void setCapacidade(Long capacidade) {
+        this.capacidade = capacidade;
+    }
+}
